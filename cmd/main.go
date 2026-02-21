21253b9599755076eac026317242ae7bce5e4778
Max15-cyber <max80021151@gmail.com>
2026-02-21 13:58:26 +0300

    feat: db connected

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+
+	"subservice/internal/config"
+	"subservice/internal/repository"
 )
 
 func main() {
+	cfg := config.Load()
+
+	db, err := repository.NewDB(
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+	)
+	if err != nil {
+		log.Fatal("failed to connect to database:", err)
+	}
+
+	log.Println("database connected")
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	r.Run(":8080")
+	r.Run(":" + cfg.Port)
+
+	_ = db
 }
---
+++ internal/repository/db.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDB(host, port, user, password, dbname string) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		user, password, host, port, dbname,
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
